71e66ae6303a52f570830235ebcb88b02700106a
GSergey <example@gmail.com>
2021-08-25 01:16:57 +0300

    init: brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let conf = {}
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    conf[bracketsConfig[i][0]] = bracketsConfig[i][1];
+  }
+  for (i = 0; i < str.length; i++) {
+    if (Object.keys(conf).includes(str[i])) {
+      stack.push(str[i]);
+    } else {
+      if (stack.length === 0) {
+        return false;
+      }
+      if (conf[str[i]] === stack[stack.length - 1]) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
 }
