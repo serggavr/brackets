6c7974a369392bdb60b122097840a89fd8e6b812
GSergey <example@gmail.com>
2021-08-27 00:38:19 +0300

    fix: algoritm

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
   let stack = [];
-  let conf = {}
+  let conf = {};
   for (let i = 0; i < bracketsConfig.length; i++) {
     conf[bracketsConfig[i][0]] = bracketsConfig[i][1];
   }
   for (let i = 0; i < str.length; i++) {
     if (Object.keys(conf).includes(str[i])) {
-      stack.push(str[i]);
+      console.log(stack[stack.length - 1])
+      console.log(str[i])
+      if (stack.length >= 1 && (stack[stack.length - 1] === Object.keys(conf).find(key => conf[key] === str[i]))) {
+        stack.pop()
+        } else {
+          stack.push(str[i]);}
     } else {
       if (stack.length === 0) {
         return false;
@@ -18,13 +23,6 @@ module.exports = function check(str, bracketsConfig) {
       }
     }
   }
-  if (stack.length >= 2) {
-      for (let i = 0; i < stack.length; i++) {
-    if (Object.keys(conf).find(key => conf[key] === stack[i]) === stack[i]) {
-        stack.splice(0, 2);
-      }
-    }
-  }
   return stack.length === 0;
 
-}
+};
